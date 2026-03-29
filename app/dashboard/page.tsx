f7c3b26d0bb4a4caad95a67e01d1605c44c28113
M Praneeth <madasu.praneeth464@gmail.com>
2026-03-29 23:54:25 +0530

    feat: initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardPage = () => {
+    return (
+        <div className='font-oxanium text-9xl p-4 flex items-center justify-center h-screen'>
+            <div className="bg-[#000] w-1/2 h-1/2"></div>
+        </div>
+    )
+}
+
+export default DashboardPage
